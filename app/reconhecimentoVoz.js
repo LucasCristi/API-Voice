6e0983a7c6d4595b14aa5d9eacde13ae4d3e91c3
Lucas Cristi <lucasccristi@gmail.com>
2022-10-11 13:10:33 -0300

    Project using voice API to guess a secret number

---
+++ app/reconhecimentoVoz.js
@@ -0,0 +1,26 @@
+const elementoChute = document.getElementById("chute")
+
+//importado do web speech API pesquisado no google
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition; 
+
+const recognition = new SpeechRecognition()
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak) 
+//importado do web speech API pesquisado no google
+
+function onSpeak(e){ //o "e" refere-se ao evento que vem do results no console.log
+    console.log(e.results[0][0].transcript) //acessa os DOIS ARRAYS que estão no log na posição 0
+
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    VerificaSechutePossuiValorValido(chute)
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box">${chute}</span>
+    `
+}
